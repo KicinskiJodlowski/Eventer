447447849f5d2f51f08961c123aec6fab0c0f6a3
Kiciński-Jodłowski Kamil <kamil.kicinski@student.wat.edu.pl>
2019-05-08 21:07:39 +0200

    login and register activity on myEvent fragment

--- app/src/main/java/com/example/eventer/LoginFragment.java
+++ app/src/main/java/com/example/eventer/LoginFragment.java
@@ -8,15 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MyEvents extends Fragment {
+public class LoginFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.my_events_fragment, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        return inflater.inflate(R.layout.login_fragment, container, false);
     }
 }
