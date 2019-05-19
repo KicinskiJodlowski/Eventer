deb1e5bb0923b69a6957c83447939f6e2f1e72c1
Kamil <kamil.kicinski@gmail.com>
2019-05-19 17:16:01 +0200

    dzialające zapisywanie sesji i reagujace na wygaśnięcie tokena

--- app/src/main/java/com/example/eventer/activity/InitialActivity.java
+++ app/src/main/java/com/example/eventer/activity/InitialActivity.java
@@ -7,8 +7,17 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.eventer.R;
+import com.example.eventer.RetrofitClient;
 import com.example.eventer.SharedPreferenceManager;
 import com.example.eventer.fragment.LoginFragment;
+import com.example.eventer.model.EventModel;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -21,38 +30,45 @@ public class InitialActivity extends AppCompatActivity {
         SharedPreferenceManager.init(getApplicationContext());
         Intent activityIntent;
 
-        // go straight to main if a token is stored
+        if (checkTokenIsValid() == true) {
 
-//        if (checkTokenIsValid() == true) {
-//
-//            activityIntent = new Intent(this, MainActivity.class);
-//
-//        } else {
-//
-//            activityIntent = new Intent(this, LoginActivity.class);
-//
-//        }
+            activityIntent = new Intent(this, MainActivity.class);
 
-        activityIntent = new Intent(this, LoginActivity.class);
+        } else {
 
+            activityIntent = new Intent(this, LoginActivity.class);
+
+        }
         startActivity(activityIntent);
-
         finish();
 
     }
 
     private boolean checkTokenIsValid() {
-        SharedPreferences sharedPreferences = this.getSharedPreferences("token", Context.MODE_PRIVATE);
-        String session = sharedPreferences.getString("token", null);
-        if (session == null) {
+
+        String session = SharedPreferenceManager.read(SharedPreferenceManager.TOKEN, "");
+        if (session.equals("") || session == null) {
             return false;
         } else {
-            //tu spr czy rzuca unauthorized dla tego tokena
-            if (true) {
-                LoginFragment.userTOKEN = "Bearer " + session; return true;
-            } else {
-                LoginFragment.userTOKEN = "Bearer " + session; return true;
-            }
+            //SharedPreferenceManager.write(SharedPreferenceManager.TOKEN, "Bearer " +"JakiToZlyTokeneyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJhOTg4NTE5NS1hMTQzLTRhYTAtYjFhMS1iMzhjOGMzYWQ2MmMiLCJuYmYiOjE1NTgyNzQ5NjEsImV4cCI6MT");
+            return true;
+//            //tu spr czy rzuca unauthorized dla tego tokena
+//            Call<ArrayList<EventModel>> call = RetrofitClient.getInstance().getApi().getEvents(SharedPreferenceManager.read(SharedPreferenceManager.TOKEN,""));
+//            call.enqueue(new Callback<ArrayList<EventModel>>() {
+//                @Override
+//                public void onResponse(Call<ArrayList<EventModel>> call, Response<ArrayList<EventModel>> response) {
+//
+//                }
+//
+//                @Override
+//                public void onFailure(Call<ArrayList<EventModel>> call, Throwable t) {
+////                    if (response.code() == 401 ){
+////                        SharedPreferenceManager.remove(SharedPreferenceManager.TOKEN);
+////                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerLogin, new LoginFragment()).commit();
+////                    }
+//                }
+//            });
+//            return true;
         }
 //        return false;
     }
--- app/src/main/java/com/example/eventer/fragment/MyEventsFragment.java
+++ app/src/main/java/com/example/eventer/fragment/MyEventsFragment.java
@@ -1,5 +1,6 @@
 package com.example.eventer.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +17,8 @@ import android.widget.Toast;
 import com.example.eventer.R;
 import com.example.eventer.RetrofitClient;
 import com.example.eventer.SharedPreferenceManager;
+import com.example.eventer.activity.LoginActivity;
+import com.example.eventer.activity.MainActivity;
 import com.example.eventer.adapter.EventRecordAdapter;
 import com.example.eventer.model.EventModel;
 
@@ -49,17 +52,13 @@ public class MyEventsFragment extends Fragment {
 
     private void getEvents() {
 
-        Call<ArrayList<EventModel>> call = RetrofitClient.getInstance().getApi().getEvents(SharedPreferenceManager.read(SharedPreferenceManager.TOKEN,""));
-
+        Call<ArrayList<EventModel>> call = RetrofitClient.getInstance().getApi().getEvents(SharedPreferenceManager.read(SharedPreferenceManager.TOKEN, ""));
         call.enqueue(new Callback<ArrayList<EventModel>>() {
             @Override
             public void onResponse(Call<ArrayList<EventModel>> call, Response<ArrayList<EventModel>> response) {
 
                 Log.d("Response Code: ", Integer.toString(response.code()));
-
-                Toast.makeText(getActivity(), response.body().toString(), Toast.LENGTH_SHORT).show();
-
-                if(response.code() == 200) {
+                if (response.code() == 200) {
 
                     Toast.makeText(getActivity(), "Pobrano wydarzenia", Toast.LENGTH_SHORT).show();
 
@@ -71,18 +70,19 @@ public class MyEventsFragment extends Fragment {
                             showEventDetails(position);
                         }
                     });
-                }
-                else {
+                    ArrayList<EventModel> events = response.body();
+
+                    for (EventModel event : events) {
+                        //if(event.getEventName() == null) event.setEventName("BRAK NAZWY"); // to prevent error with events with no title
+                        listEvents.add(event); //tested - OK
+                    }
+                } else {
                     Toast.makeText(getActivity(), "Wystąpił błąd! Nie udało się pobrać wydarzeń.", Toast.LENGTH_SHORT).show();
-                }
-
-
-                ArrayList<EventModel> events = response.body();
-
-                for(EventModel event : events)
-                {
-                    //if(event.getEventName() == null) event.setEventName("BRAK NAZWY"); // to prevent error with events with no title
-                    listEvents.add(event); //tested - OK
+                    if (response.code() == 401) {
+                        SharedPreferenceManager.remove(SharedPreferenceManager.TOKEN);
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
                 }
             }
 
@@ -95,7 +95,7 @@ public class MyEventsFragment extends Fragment {
     }
 
     private void showEventDetails(int position) {
-        FragmentTransaction ft =  getActivity().getSupportFragmentManager().beginTransaction();
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         EventDetailsFragment detailsFragment = new EventDetailsFragment();
 
